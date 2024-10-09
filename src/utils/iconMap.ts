df0ef946cd3068f5c9eb00dd46e5ecb7a49a809c
Robert Schimanek <robert.schimanek@icloud.com>
2024-10-09 14:39:59 +0200

    Feature: Youtube 100 Second videos if clicked on icon

---
+++ src/utils/iconMap.ts
@@ -0,0 +1,30 @@
+export const iconYouTubeLinks = new Map([
+  // Language icons
+  ['i-simple-icons-c', 'https://www.youtube.com/watch?v=U3aXWizDbQ4'],
+  ['i-simple-icons-cplusplus', 'https://www.youtube.com/watch?v=MNeX4EGtR5Y'],
+  ['i-simple-icons-python', 'https://www.youtube.com/watch?v=x7X9w_GIm1s'],
+  ['i-simple-icons-javascript', 'https://www.youtube.com/watch?v=DHjqpvDnNGE'],
+  ['i-simple-icons-typescript', 'https://www.youtube.com/watch?v=zQnBQ4tB3ZA'],
+  ['i-simple-icons-html5', 'https://www.youtube.com/watch?v=ok-plXXHlWw'],
+  ['i-simple-icons-mysql', 'https://www.youtube.com/watch?v=zsjvFFKOm3c'],
+
+  // Framework icons
+  ['i-simple-icons-tensorflow', 'https://www.youtube.com/watch?v=i8NETqtGHms'],
+  ['i-simple-icons-pytorch', 'https://www.youtube.com/watch?v=ORMx45xqWkA'],
+  ['i-akar-icons-vue-fill', 'https://www.youtube.com/watch?v=nhBVL41-_Cw'],
+  ['i-simple-icons-astro', 'https://www.youtube.com/watch?v=dsTXcSeAZq8'],
+
+  // Technology icons
+  ['i-simple-icons-linux', 'https://www.youtube.com/watch?v=rrB13utjYV4'],
+  ['i-simple-icons-docker', 'https://www.youtube.com/watch?v=Gjnup-PuquQ'],
+  ['i-simple-icons-git', 'https://www.youtube.com/watch?v=hwP7WQkmECE'],
+  ['i-simple-icons-redis', 'https://www.youtube.com/watch?v=G1rOthIU-uo'],
+  ['i-simple-icons-pandas', 'https://www.youtube.com/watch?v=dcqPhpY7tWk'],
+  ['i-simple-icons-flask', 'https://www.youtube.com/watch?v=mpYI_oPJdTA'],
+  ['i-simple-icons-influxdb', 'https://www.youtube.com/watch?v=Lpz9sRLjfvE'],
+  ['i-simple-icons-grafana', 'https://www.youtube.com/watch?v=lILY8eSspEo'],
+  ['i-teenyicons-azure-solid', 'https://www.youtube.com/watch?v=YfZ0zk5Zzcw'],
+
+  ['i-simple-icons-lodash', 'https://www.youtube.com/watch?v=yswr0G3kK14'],
+
+])
